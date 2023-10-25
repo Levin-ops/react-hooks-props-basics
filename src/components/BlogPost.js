7015089dd05da0ebe87232e7dc46498087811485
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-25 14:09:02 +0300

    Deleted and coded along

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,12 +5,17 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <BlogContent 
+      isPublished = {true}
+      minutesToRead = {1}
+      articleText = "Dear Reader: Bjarne Stroustrup has the perfect lecture oration" />
+      <Comment commentText = "I Agree with this statement. -Angela Merkel"/>
+      <Comment commentText = "A universal truth. - Noam Chomsky"/>
+      <Comment commentText = "Truth is singular. Its 'Versions' are mistruths - Sonmi-451"/>
     </div>
   );
 }
 
 export default BlogPost;
+
+// Parent Component
